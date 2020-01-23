d678d6747d89f626b1391c67be529b4102e9d89f
kotvitaliy <kotvitaliy8@gmail.com>
2020-01-23 15:29:36 +0200

    first commit

---
+++ src/main/java/com/kvi/automation/App.java
@@ -0,0 +1,13 @@
+package com.kvi.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
